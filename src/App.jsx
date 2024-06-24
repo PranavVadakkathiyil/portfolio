2db94f62f879701b75df5e39dc9ee26dd48fd7ea
Pranav Vadakkathiyil <vpranav0801@gmail.com>
2024-06-24 23:54:56 +0530

    project page design

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
   return (
     <div>
       <Header/>
+      {/*<About/>*/}
       <Projects/>
       {/*<Profile/>*/}
       {/*<Banner/>*/}
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,39 +1,74 @@
 import React from "react";
-import banner from "../Img/banner.png";
-import mainimg from "../Img/main.svg";
-import img from "../Img/pranav2.1.png";
 import {
-    FaGithub,FaLink 
+    FaLink 
   } from "react-icons/fa";
 const Projects = () => {
   return (
-    <div>
-      <div className="grid sm:grid-cols-3 grid-cols-1 m-4 gap-3 ">
-        <div className="col-span-1 border-4   rounded-tl-[4rem] h-[40vh] flex flex-col  justify-center p-4">
-          <div className="border-4 h-[60%]  rounded-tl-[4rem] m-3 flex justify-center">
-            <img src={banner} alt="img" className="h-full " />
+    <div className=" flex items-center  sm:m-3">
+      <div className="grid sm:grid-cols-4 grid-cols-1 m-2 gap-3  ">
+        <p className="font-[2rem] text-[1.6rem] sm:text-[2.2rem]  text-center p-1 sm:flex items-center justify-center bg-gradient-to-r from-zinc-800 to-zinc-900 text-white sm:rounded-[2rem]">Projects</p>
+      <div className="col-span-1  bg-neutral-900  rounded-[2rem] h-[60vh]  max-w-sm flex flex-col  justify-center p-5">
+          <div className=" h-[70%] bg-black rounded-[2rem] sm:m-1 flex justify-center">
+            <p className="text-white text-[2rem] text-center sm:p-0 p-1 text-wrap flex items-center">Lorem ipsum dolor sit.</p>
           </div>
-          <div className="flex justify-end pr-5 gap-5">
-            <p><FaGithub className="text-[1.4rem]"/></p>
-            <p><FaLink className="text-[1.4rem]"/></p>
+          <div className="flex items-center justify-between  m-3 ">
+            
+              <div>
+                <p className="text-gray-500  font-light text-[1.5rem]">lorem</p>
+                <p className="text-[23px] text-[#5B78F6] leading-[1.7rem]">Project Lorem, ipsum.</p>
+              </div>
+              <div className="flex flex-col gap-2  ">
+              <p><FaLink className="text-[1.4rem] text-white cursor-pointer"/></p>
+              </div>
+            
           </div>
         </div>
-        <div className="col-span-1 border-4   rounded-tl-[4rem] h-[40vh] flex flex-col  justify-center p-4">
-          <div className="border-4 h-[60%]  rounded-tl-[4rem] m-3 flex justify-center">
-            <img src={banner} alt="img" className="h-full " />
+        <div className="col-span-1  bg-neutral-900  rounded-[2rem] h-[60vh]  max-w-sm flex flex-col  justify-center p-5">
+          <div className=" h-[70%] bg-black rounded-[2rem] sm:m-1 flex justify-center">
+            <p className="text-white text-[2rem] text-center sm:p-0 p-1 text-wrap flex items-center">Lorem ipsum dolor sit.</p>
           </div>
-          <div className="flex justify-end pr-5 gap-5">
-            <p><FaGithub className="text-[1.4rem]"/></p>
-            <p><FaLink className="text-[1.4rem]"/></p>
+          <div className="flex items-center justify-between  m-3 ">
+            
+              <div>
+                <p className="text-gray-500  font-light text-[1.5rem]">lorem</p>
+                <p className="text-[23px] text-[#5B78F6] leading-[1.7rem]">Project Lorem, ipsum.</p>
+              </div>
+              <div className="flex flex-col gap-2  ">
+              <p><FaLink className="text-[1.4rem] text-white cursor-pointer"/></p>
+              </div>
+            
           </div>
         </div>
-        <div className="col-span-1 border-4   rounded-tl-[4rem] h-[40vh] flex flex-col  justify-center p-4">
-          <div className="border-4 h-[60%]  rounded-tl-[4rem] m-3 flex justify-center">
-            <img src={banner} alt="img" className="h-full " />
+        <div className="col-span-1  bg-neutral-900  rounded-[2rem] h-[60vh]  max-w-sm flex flex-col  justify-center p-5">
+          <div className=" h-[70%] bg-black rounded-[2rem] sm:m-1 flex justify-center">
+            <p className="text-white text-[2rem] text-center sm:p-0 p-1 text-wrap flex items-center">Lorem ipsum dolor sit.</p>
           </div>
-          <div className="flex justify-end pr-5 gap-5">
-            <p><FaGithub className="text-[1.4rem]"/></p>
-            <p><FaLink className="text-[1.4rem]"/></p>
+          <div className="flex items-center justify-between  m-3 ">
+            
+              <div>
+                <p className="text-gray-500  font-light text-[1.5rem]">lorem</p>
+                <p className="text-[23px] text-[#5B78F6] leading-[1.7rem]">Project Lorem, ipsum.</p>
+              </div>
+              <div className="flex flex-col gap-2  ">
+              <p><FaLink className="text-[1.4rem] text-white cursor-pointer"/></p>
+              </div>
+            
+          </div>
+        </div>
+        <div className="col-span-1  bg-neutral-900  rounded-[2rem] h-[60vh]  max-w-sm flex flex-col  justify-center p-5">
+          <div className=" h-[70%] bg-black rounded-[2rem] sm:m-1 flex justify-center">
+            <p className="text-white text-[2rem] text-center sm:p-0 p-1 text-wrap flex items-center">Lorem ipsum dolor sit.</p>
+          </div>
+          <div className="flex items-center justify-between  m-3 ">
+            
+              <div>
+                <p className="text-gray-500  font-light text-[1.5rem]">lorem</p>
+                <p className="text-[23px] text-[#5B78F6] leading-[1.7rem]">Project Lorem, ipsum.</p>
+              </div>
+              <div className="flex flex-col gap-2  ">
+              <p><FaLink className="text-[1.4rem] text-white cursor-pointer"/></p>
+              </div>
+            
           </div>
         </div>
       </div>
